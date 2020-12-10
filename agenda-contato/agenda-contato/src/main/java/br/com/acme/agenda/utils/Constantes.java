893a3873a36a5cca4ebc1eee24c8ec0dc260414b
veihmeyer <veihmeyer@hotmail.com>
2020-12-10 15:54:19 -0300

    Projeto Tec. Web Final

---
+++ agenda-contato/agenda-contato/src/main/java/br/com/acme/agenda/utils/Constantes.java
@@ -0,0 +1,12 @@
+
+package br.com.acme.agenda.utils;
+
+
+public class Constantes {
+
+	public static final String PERSISTENCE_UNIT_NAME = "ACME";
+	
+	
+	
+	public static final String LISTAR_CONTATOS = "/pages/cadastros/contatos/lista_contatos.jsp";
+}
